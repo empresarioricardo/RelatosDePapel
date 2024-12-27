542698993308b413b9b1aa416c8b1025a739d703
empresarioricardo <51102071+empresarioricardo@users.noreply.github.com>
2024-12-27 18:35:00 -0500

    Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import imagen404 from "../assets/images/not-found-error.jpg";
 const NotFound = () => {
   return (
-    <div>
+    <div className="text-center vh-100 d-flex flex-column justify-content-center align-items-center">
       <h1>ERROR 404</h1>
-      <p>NotFound</p>
+      <hr />
+      <h2>Recurso no encontrado!!</h2>
+      <img src={imagen404} alt="Error" />
     </div>
   );
 };
